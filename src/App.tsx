a4ef2e5378b483c333f6a3a37572e95a064d1e88
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 11:15:27 +0100

    list/for

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,12 @@ function App() {
 				<button className="info info-disabled">Info</button>
 			</section>
 			<hr/>
+			<h1 className="special">The H1</h1>
+			<h2 className="special">The H2</h2>
+			<h3 className="special">The H3</h3>
+			<h4 className="special">The H4</h4>
+			<h5 className="special">The H5</h5>
+			<h6 className="special">The H6</h6>
 
 		</div>
 	);
