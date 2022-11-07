2f10fec01431478ee154c79be68f2a1b90ecaaf5
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 09:40:51 +0100

    variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
 		<div className="App">
 			<h1>Info Site</h1>
 			<p>Welcome to this site.</p>
+			<h2>Detailed Info</h2>
+			<p>ksjdfksdjflsdfj sdkfj sdkj sdkfjd f</p>
+			<h3>More Info</h3>
+			<p>ksjdfksdjflsdfj sdkfj sdkj sdkfjd f</p>
+			
 		</div>
 	);
 }
